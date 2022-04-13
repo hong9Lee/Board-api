b5de2876c982e64fae6bc3458abd03ca7cbfa267
hong9Lee <hongtemp9mac@gmail.com>
2022-04-13 19:42:08 +0900

    Service, Controller 추가 (login, addUser)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Logger, Post } from '@nestjs/common';
+import {UserService} from "./user.service";
+import {Register, UserInfo, Login} from "./user.type";
+import {loginSchema, registerSchema} from "./user.schema";
+import {ResponseMessage, Response} from "../util/response.util";
+import {ValidationError} from "joi";
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    @Post("register")
+    public async addUser(@Body() register: Register): Promise<Response> {
+        try {
+            const { value, error }: {
+                value: Register,
+                error?: ValidationError;
+            } = registerSchema.validate(register);
+
+            if(error){
+                Logger.error(error);
+                return new ResponseMessage().error(999).body("Parameter Error").build();
+            }
+
+            const user: UserInfo = await this.userService.addUser(value);
+            return new ResponseMessage().success().body(user).build();
+        } catch (err){
+            Logger.error(err);
+        }
+    }
+
+    @Post('login')
+    public async login(@Body() login: Login): Promise<Response> {
+        const { value, error }: {
+            value: Login,
+            error?: ValidationError;
+        } = loginSchema.validate(login);
+
+        if (error) {
+            Logger.error(error);
+            return new ResponseMessage().error(999).body("Parameter Error").build();
+        }
+
+        const user = await this.userService.login(value);
+
+        if(!user) return new ResponseMessage().error(999, "Login Error").build();
+
+        return new ResponseMessage().success().body(user).build();
+    }
+
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {UserRepository} from "./user.repository";
+import {Register, UserInfo, Login, LoginUserInfo} from "./user.type";
+import * as Bcrypt from 'bcryptjs';
+import {Token} from 'src/util/token.util';
+import {User} from "../entities/user.entity";
+
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private readonly userRepository: UserRepository) {
+    }
+
+    /** User 생성 */
+    public async addUser(register: Register): Promise<UserInfo> {
+        const registerUser = await this.userRepository.create();
+
+        const salt: string = await Bcrypt.genSalt(10);
+        const password: string = await Bcrypt.hash(register.password, salt);
+
+        registerUser.email = register.email;
+        registerUser.name = register.name;
+        registerUser.uuid = Token.getUUID();
+        registerUser.password = password;
+
+
+        const user = await this.userRepository.save(registerUser);
+        const userInfo: UserInfo = {
+            email: user.email,
+            name: user.name,
+            uuid: user.uuid
+        };
+        return userInfo;
+    }
+
+    /** User Login 생성 */
+    public async login(loginUser: Login): Promise<LoginUserInfo> {
+        // find user in db
+        const user: User = await this.userRepository.findOne({
+            where: {
+                email: loginUser.email
+            }
+        });
+
+        // password 체크
+        const passwordCheck = await Bcrypt.compare(loginUser.password, user.password);
+        if(!passwordCheck) return null;
+
+        user.lastLoginDate = new Date(); // set last login time
+        await this.userRepository.save(user);
+
+        const userInfo: LoginUserInfo = {
+            email: user.email,
+            name: user.name,
+            uuid: user.uuid,
+            lastLogin: user.lastLoginDate
+        };
+
+        return userInfo;
+    }
+
+
+}
